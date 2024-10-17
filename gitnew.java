e7eb7798671f06a68a5f2d291f31501e3a3b7028
Kumar <ekekkg@gmail.com>
2024-10-17 23:33:18 +0530

    new files

---
+++ gitnew.java
@@ -0,0 +1,16 @@
+
+public class gitnew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I will write the code");
+		
+		System.out.println("I will write the code");
+		System.out.println("I will write the code");
+		System.out.println("I will write the code");
+		
+
+	}
+
+}
